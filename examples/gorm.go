aaf24a1659cbd807119f1470d025d574b9184176
Khoa Nguyen <khoa.nguyen@snapmart.ph>
2021-05-31 22:32:27 +0700

    init

---
+++ examples/gorm.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/best-expendables-v2/newrelic-context/nrgorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initDB() *gorm.DB {
+	dsn := "host=%s user=%s port=%d dbname=%s sslmode=disable password=%s"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	nrgorm.AddGormCallbacks(db)
+	return db
+}
